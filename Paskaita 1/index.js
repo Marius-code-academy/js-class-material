41a20e7071c6238dcf5cc029fb49e82021e4e60d
Marius <marius.matulevicius1@gmail.com>
2023-07-24 20:06:14 +0300

    paskaita 1

---
+++ Paskaita 1/index.js
@@ -0,0 +1,37 @@
+// console.log();
+
+const PI_VALUE = -3.14;
+
+const name = "Marius";
+let age = 26;
+let lastName = "asdf";
+console.log(lastName);
+
+console.log(age);
+age = 30;
+console.log(age);
+
+console.log(name);
+
+const sum = 10 + 15;
+console.log(sum);
+
+const sub = 10 - 7;
+console.log(sub);
+
+const mult = 3 * 8;
+console.log(mult);
+
+const div = 6 / 2;
+console.log(div);
+
+const sum1 = "Labas" + "Ate" + "asdfadsf";
+console.log(sum1);
+
+const sub1 = "labas" * "Ate";
+console.log(sub1);
+
+const sub3 = age + 7;
+console.log(sub3);
+console.log(age);
+console.log(6 + 8);
---
+++ Paskaita 1/uzduotis1/index.js
@@ -0,0 +1,22 @@
+// ------------- Uzduotis 1
+
+// console.log("Hello World!");
+// alert("I Love Javascript!");
+// alert("Ready to learn more?");
+// const number = prompt("Ivesk skaiciuo nuo 1 iki 10");
+// console.log(number);
+
+// ------------- Uzduotis 2
+// const name = prompt("Iveskite savo varda");
+// const birthDay = "Geguzes 12";
+
+// const fullText = name + " yra gimes " + birthDay;
+// console.log(fullText);
+
+// -------------- Uzduotis 3
+
+const bazinisUzmokestis = prompt(" Iveskite bazini uzmokesti ");
+const workingPeople = 1761463;
+const budget = 8487300000;
+const precent = (workingPeople * bazinisUzmokestis * 100) / budget;
+document.write(precent);
