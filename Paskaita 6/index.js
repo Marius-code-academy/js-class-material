4aa8db16796e7a03e501ad3ac34d7522829aa27e
Marius <marius.matulevicius1@gmail.com>
2023-08-01 19:30:01 +0300

    Paskaita 6

--- Paskaita 6/index.js
+++ Paskaita 6/index.js
@@ -3,9 +3,41 @@ const ageInput = document.querySelector("input[type=number]");
 const commentInput = document.querySelector("input[type=text]");
 const driversLicenseCheckboxInput = document.querySelector("input[type=checkbox]");
 const makeSelect = document.querySelector("#make");
+const sayHelloButton = document.querySelector("#sayHelloButton");
+const h1 = document.querySelector("h1");
+const passwordInput = document.querySelector("input[type=password]");
 
 document.querySelector("#someForm").addEventListener("submit", handleSubmit);
 driversLicenseCheckboxInput.addEventListener("change", handleCheckboxChange);
+sayHelloButton.addEventListener("click", sayHelloHandler);
+passwordInput.addEventListener("input", passwordChangeHandler);
+
+function passwordChangeHandler(e) {
+  const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/;
+  const password = e.target.value;
+
+  const isStrongPassword = regex.test(password);
+
+  if (isStrongPassword) {
+    passwordInput.classList.add("good");
+    passwordInput.classList.remove("bad");
+  } else {
+    passwordInput.classList.add("bad");
+    passwordInput.classList.remove("good");
+  }
+
+  if (!password) {
+    passwordInput.classList.remove("good");
+    passwordInput.classList.remove("bad");
+  }
+}
+
+function sayHelloHandler() {
+  const age = ageInput.value;
+  const comment = commentInput.value;
+
+  h1.textContent = `jusu amzius yra ${age} ir jusu nuome apie pausauli yra ${comment}`;
+}
 
 function handleSubmit(event) {
   event.preventDefault();
