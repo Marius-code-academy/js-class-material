fc295fd0cd32552b19be5e495718e77ce035dc73
Marius <marius.matulevicius1@gmail.com>
2023-08-21 18:39:27 +0300

    paskaita 16

---
+++ Paskaita 16/index.js
@@ -0,0 +1,51 @@
+class Book {
+  constructor(title, year, id, author) {
+    this.title = title;
+    this.id = id;
+    this.year = year;
+    this.author = author;
+  }
+
+  logTitle() {
+    console.log(this.title);
+  }
+}
+
+class Library {
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+    this.books = [];
+  }
+
+  addNewBook(book) {
+    this.books.push(book);
+  }
+
+  findBookByTitle(title) {
+    return this.books.find((book) => book.title === title);
+  }
+
+  removeBookById(id) {
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+    console.log(bookIndex);
+    if (bookIndex !== -1) {
+      this.books.splice(bookIndex, 1);
+    } else {
+      console.log("Knygos su tokiu id nera");
+    }
+  }
+}
+
+const martynoMazvydo = new Library("Martyno Mazvydo", "prie seimo");
+
+const kvepalai = new Book("Kvepalai", 2000, 1, "kazkoks autorius");
+const altoriuSesely = new Book("Altoriu Sesely", 1990, 2, "Kazkas");
+
+martynoMazvydo.addNewBook(kvepalai);
+martynoMazvydo.addNewBook(altoriuSesely);
+
+console.log(martynoMazvydo.findBookByTitle("Kvepalai"));
+martynoMazvydo.removeBookById(1);
+
+console.log(martynoMazvydo);
